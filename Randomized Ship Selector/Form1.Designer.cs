140d964fc0cb690cdb128be3d3b71baa997fdfb5
Daan Hofmans <Gebruiker@DESKTOP-Q42037P>
2018-01-27 14:02:27 +0100

    Renamed elements

--- Randomized Ship Selector/Form1.Designer.cs
+++ Randomized Ship Selector/Form1.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(749, 529);
+            this.Name = "Form1";
+            this.Text = "Random Ship Selector";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
